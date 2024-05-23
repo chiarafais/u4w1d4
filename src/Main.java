4e59f848c94629f0f69b90ef1c5496be91a8d2e9
chiarafais <chiara0915@gmail.com>
2024-05-23 16:29:43 +0200

    es1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import enums.Dipartimenti;
+import esercizi.Dipendente;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Dipendente Mari = new Dipendente(Dipartimenti.PRODUZIONE);
+        Dipendente Fra = new Dipendente(Dipartimenti.VENDITE);
+        Dipendente Luca = new Dipendente(Dipartimenti.AMMINISTRAZIONE);
+
+        Dipendente[] listaDipendenti = {Mari, Fra, Luca};
+
+
+        for (int i = 0; i < listaDipendenti.length; i++) {
+            Dipendente dipendente = listaDipendenti[i];
+            System.out.println(dipendente.getMatricola());
+        }
     }
 }
--- src/esercizi/Dipendente.java
+++ src/esercizi/Dipendente.java
@@ -1,4 +1,39 @@
 package esercizi;
 
+import enums.Dipartimenti;
+
+import java.util.Random;
+
 public class Dipendente {
+
+
+    protected int matricola;
+    protected int  stipendio;
+    protected Dipartimenti dipartimento;
+
+
+    public Dipendente(Dipartimenti dipartimento) {
+        Random rndm = new Random();
+        this.matricola = rndm.nextInt(1000, 5000);
+        this.stipendio = 1000;
+        this.dipartimento = dipartimento;
+
+    }
+
+
+    public void setDipartimento(Dipartimenti dipartimento) {
+        this.dipartimento = dipartimento;
+    }
+
+    public int getStipendio() {
+        return stipendio;
+    }
+
+    public Dipartimenti getDipartimento() {
+        return dipartimento;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
 }
